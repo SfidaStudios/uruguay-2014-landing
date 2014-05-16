996a62ce8460617efc3147812f49cc23ee1457a1
druellan <druellan@ecimtech.com>
2014-05-16 17:41:00 -0300

    Primer commit!

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+
+	$("#big-uruguay").fitText();
+	$("#big-sfida").fitText();
+
+	// Letraslocas
+	var viewSize = $("body").width();
+	var biguruSize = $("#big-uruguay div").width();
+	var bigsfidaSize = $("#big-sfida div").width();
+
+	$("#big-uruguay")
+	.css({ overflow: "hidden", width: "100%", height: "100%" })
+	.find("div")
+	.css({ width: biguruSize });
+
+	$("#big-sfida")
+	.css({ overflow: "hidden", width: "100%", height: "100%" })
+	.find("div")
+	.css({ width: bigsfidaSize });
+
+	animaticUruguay();
+	animaticSfida();
+
+	function animaticUruguay()
+	{
+		$("#big-uruguay div").css({ left: 0 });
+		$("#big-uruguay div").animate({ left: -(biguruSize/2.2) }, 40000, "linear", function(){ animaticUruguay(); });
+	}
+
+	function animaticSfida()
+	{
+		$("#big-sfida div").css("left", -(bigsfidaSize/2.2));
+		$("#big-sfida div").animate({ left: 0 }, 60000, "linear", function(){ animaticSfida(); });
+	}
+
+});
