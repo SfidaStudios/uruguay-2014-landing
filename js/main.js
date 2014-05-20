058aa0a6f05763f2da934c0779a330118e44ce4a
druellan <druellan@ecimtech.com>
2014-05-20 20:52:24 -0300

    Conseguimos un escalado dinámico de las fuentes

--- js/main.js
+++ js/main.js
@@ -1,36 +1,46 @@
 $(function(){
 
-	$("#big-uruguay").fitText();
-	$("#big-sfida").fitText();
-
-	// Letraslocas
-	var viewSize = $("body").width();
-	var biguruSize = $("#big-uruguay div").width();
-	var bigsfidaSize = $("#big-sfida div").width();
-
-	$("#big-uruguay")
-	.css({ overflow: "hidden", width: "100%", height: "100%" })
-	.find("div")
-	.css({ width: biguruSize });
-
-	$("#big-sfida")
-	.css({ overflow: "hidden", width: "100%", height: "100%" })
-	.find("div")
-	.css({ width: bigsfidaSize });
-
-	animaticUruguay();
-	animaticSfida();
-
-	function animaticUruguay()
-	{
-		$("#big-uruguay div").css({ left: 0 });
-		$("#big-uruguay div").animate({ left: -(biguruSize/2) }, 40000, "linear", function(){ animaticUruguay(); });
-	}
-
-	function animaticSfida()
-	{
-		$("#big-sfida div").css("left", -(bigsfidaSize/2));
-		$("#big-sfida div").animate({ left: 0 }, 60000, "linear", function(){ animaticSfida(); });
-	}
-
+    $(window).on('resize orientationchange', resizer);
+    resizer();
 });
+
+function resizer()
+{
+    // Tomamos el tamaño del viewport
+    var viewSize = $("body").width();
+    
+    // Calculamos el tamaño ideal de la fuente
+    var fontSize = Math.max(Math.min(viewSize, 1100), parseFloat(500));
+    $("#big-uruguay div").css("font-size", fontSize);
+    $("#big-sfida div").css("font-size", fontSize);
+    
+    var biguruSize = $("#big-uruguay div").width();
+    var bigsfidaSize = $("#big-sfida div").width();
+    
+
+    $("#big-uruguay")
+    .css({ overflow: "hidden", width: "100%", height: "100%" })
+    .find("div")
+    .css({ width: biguruSize });
+
+    $("#big-sfida")
+    .css({ overflow: "hidden", width: "100%", height: "100%" })
+    .find("div")
+    .css({ width: bigsfidaSize });
+
+    animaticUruguay(biguruSize);
+    animaticSfida(bigsfidaSize);
+}
+
+
+function animaticUruguay(biguruSize)
+{
+    $("#big-uruguay div").css({ left: 0 });
+    $("#big-uruguay div").animate({ left: -(biguruSize/2) }, 40000, "linear", function(){ animaticUruguay(); });
+}
+
+function animaticSfida(bigsfidaSize)
+{
+    $("#big-sfida div").css("left", -(bigsfidaSize/2));
+    $("#big-sfida div").animate({ left: 0 }, 60000, "linear", function(){ animaticSfida(); });
+}
